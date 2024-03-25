b9168aff35641cd718ba4da83867e1f707756d6e
Mario Pardo <mario.pardo003@gmail.com>
2024-03-25 15:36:19 -0400

    Most basic user interface

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -22,12 +23,11 @@ public class Main {
     public static boolean setDbConnection()
     {
         //<editor-fold desc=" Insert Info HERE!">
-        String databaseName = "****";
+        String databaseName = "***";
         String url = "jdbc:postgresql://localhost:5432/" + databaseName;
         String user = "postgres";
-        String password = "****";
+        String password = "***";
         //</editor-fold
-
 
         try{
             Class.forName("org.postgresql.Driver");
@@ -36,7 +36,6 @@ public class Main {
             System.out.println("Problem interacting with Database! " + e.toString());
             return false;
         }
-
         //We are now connected
         return true;
     }
@@ -45,6 +44,45 @@ public class Main {
 
     public static void launch()
     {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(" *** Welcome to the MDA Fitness Club  *** ");
+
+        while (true) {
+            System.out.println("Select an option:");
+            System.out.println("1. Customer Sign In");
+            System.out.println("2. Trainer Sign In");
+            System.out.println("3. General Management");
+            System.out.println("0. Exit");
+            System.out.print("Enter your choice: ");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Customer sign in selected");
+                    //do stuff
+                    break;
+                case 2:
+                    System.out.println("Trainer sign in selected");
+                    //do stuff
+                    break;
+                case 3:
+                    System.out.println("General Management selected");
+                    //do stuff
+                    break;
+                case 0:
+                    System.out.println("Exiting application");
+                    return;
+                default:
+                    System.out.println("Invalid choice, please try again.");
+            }
+        }
+
+
+
+
+
+
     }
 }
