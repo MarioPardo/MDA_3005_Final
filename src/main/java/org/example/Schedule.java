7dbfc844ea7090f9e6e1227786cf91d1bb9cdc94
carmelocoding <dantespezza@gmail.com>
2024-03-28 00:56:39 -0400

    create schedule function

--- src/main/java/org/example/Schedule.java
+++ src/main/java/org/example/Schedule.java
@@ -2,4 +2,26 @@ package org.example;
 
 public class Schedule
 {
+
+   public static int createSchedule(Connection conn, String date) throws SQLException
+   {
+       String sql = "INSERT INTO schedule (date) VALUES (?) RETURNING id";
+
+       try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+           pstmt.setDate(1, Date.valueOf(date));
+           ResultSet rs = pstmt.executeQuery();
+
+           if (rs.next()) {
+               return rs.getInt("id");
+           }
+       }
+       return -1;
+
+   }
+
+
+
+
+
+
 }
