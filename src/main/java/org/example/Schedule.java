10a1d4e9a63bb404f39d8f00bceb2fbb2c92dec2
Mario Pardo <mario.pardo003@gmail.com>
2024-03-28 14:00:11 -0400

    fixed, slight refactor

--- src/main/java/org/example/Schedule.java
+++ src/main/java/org/example/Schedule.java
@@ -1,23 +1,27 @@
 package org.example;
 
+import java.sql.*;
+
 public class Schedule
 {
 
-   public static int createSchedule(Connection conn, String date) throws SQLException
-   {
-       String sql = "INSERT INTO schedule (date) VALUES (?) RETURNING id";
+    public static int createSchedule(String date) {
+        Connection conn = Main.dbConnection;
+        String sql = "INSERT INTO schedule (schedule_date) VALUES (?) RETURNING id";
 
-       try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-           pstmt.setDate(1, Date.valueOf(date));
-           ResultSet rs = pstmt.executeQuery();
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setDate(1, Date.valueOf(date));
+            ResultSet rs = pstmt.executeQuery();
 
-           if (rs.next()) {
-               return rs.getInt("id");
-           }
-       }
-       return -1;
-
-   }
+            if (rs.next()) {
+                return rs.getInt("id");
+            }
+        } catch (SQLException e) {
+            System.out.println("ERROR Creating Schedule");
+            e.printStackTrace();
+        }
+        return -1; // Return -1 if an exception occurs
+    }
 
 
 
