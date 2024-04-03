056e97352dae0087f4a04d4aba68e1d96a4d4d04
ahmedsayedsharafeldin <163793637+ahmedsayedsharafeldin@users.noreply.github.com>
2024-04-03 19:55:13 +0400

    rebased from main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -24,10 +23,10 @@ public class Main {
     public static boolean setDbConnection()
     {
         //<editor-fold desc=" Insert Info HERE!">
-        String databaseName = "finalproject";
+        String databaseName = "**";
         String url = "jdbc:postgresql://localhost:5432/" + databaseName;
         String user = "postgres";
-        String password = "xsixteen123";
+        String password = "**";
         //</editor-fold
 
         try{
@@ -61,26 +60,26 @@ public class Main {
             switch (choice) {
                 case 1:
                     System.out.println("Customer sign in selected");
-
                     int custID = SignIn(1);
                     if(custID == -1)
                         break;
+
                     CustomerUI(custID);
 
                     break;
                 case 2:
                     System.out.println("Trainer sign in selected");
-                    int trainerID = SignIn(1);
+                    int trainerID = SignIn(2);
                     if(trainerID == -1)
                         break;
 
-                    trainerID = 1; //for testing for now
+
                     TrainerUI(trainerID);
 
                     break;
                 case 3:
                     System.out.println("General Management selected");
-                    int mgmtID = SignIn(1);
+                    int mgmtID = SignIn(3);
                     if(mgmtID == -1)
                         break;
 
@@ -102,55 +101,125 @@ public class Main {
     public static int SignIn(int type)
     {
         System.out.print("Enter your username: ");
-        String username = scanner.nextLine();
         scanner.nextLine();
+        String username = scanner.nextLine();
+        username = username.trim();
+
         System.out.print("Enter your password: ");
         String password = scanner.nextLine();
+        password = password.trim();
 
-        int ID = 1;
+
+        int ID = -1;
 
         switch (type)
         {
-            case 1: //call sql user sign  in function
+            case 1:
+                 ID = Customer.customerSignIn(username, password);
                 break;
-            case 2:  //call sql trainer sign in function
+            case 2:
+                 ID = Trainer.TrainerSignIn(username,password);
                 break;
+
             case 3: //management sql  sign in in function
+                //TODO make customer sign in func
+                ID = 1; //temp for now
+
                 break;
         }
 
         return ID;
     }
-    
-    public static void CustomerUI(int custID)
-    {
-        System.out.println(" \n \n Welcome to your Customer profile!");
 
-        //view all profiles
 
-        //add profile
-            //TODO link this added profile to the customer
-        Profile.addProfile();
+    public static void CustomerUI(int custID) {
+        System.out.println("\n\nWelcome to your Customer profile!");
 
-        //select profile to go to
-        
+        int choice = -1;
+
+        while (choice != 0) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. View all profiles");
+            System.out.println("2. Add profile");
+            System.out.println("3. Select profile to go to");
+            System.out.println("0. Exit");
+
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    // TODO: View customer all profiles
+
+                    break;
+                case 2:
+                    Profile.addProfile(); //TODO change this func to return an id
+                    // TODO: Link this added profile to the customer
+                    break;
+                case 3:
+                    // TODO: Select profile to go to
+                    break;
+                case 0:
+                    System.out.println("Exiting Customer profile...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
 
     }
 
-    public static void ProfileUI(int profileID)
-    {
-        //view schedule
+    public static void ProfileUI(int profileID) {
+        System.out.println("\n\nWelcome to your Profile!");
 
-        //view all available group classes
+        int choice = -1;
 
-        //add group class to schedule
+        while (choice != 0) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. View schedule");
+            System.out.println("2. View all available group classes");
+            System.out.println("3. Add group class to schedule");
+            System.out.println("4. Book personal training class");
+            System.out.println("5. Remove class from schedule");
+            System.out.println("6. Change health");
+            System.out.println("0. Exit");
 
-        //book personal training class
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
-        //remove class from schedule
-
-        //change health
-
+            switch (choice) {
+                case 1:
+                    // TODO: View schedule
+                    break;
+                case 2:
+                    // TODO: View all available group classes
+                    break;
+                case 3:
+                    // TODO: Add group class to schedule
+                    break;
+                case 4:
+                    // TODO: Book personal training class
+                    System.out.println("Booking personal training class...");
+                    break;
+                case 5:
+                    // TODO: Remove class from schedule
+                    System.out.println("Removing class from schedule...");
+                    break;
+                case 6:
+                    // TODO : Change health
+                    System.out.println("Changing health...");
+                    break;
+                case 0:
+                    System.out.println("Exiting Profile...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 
     public static void TrainerUI(int trainerID)
@@ -172,19 +241,19 @@ public class Main {
 
             switch (choice) {
                 case 1:
-                    //setWorkingHours();
+                    //TODO : setWorkingHours();
                     break;
                 case 2:
-                    //showAllClients();
+                    //TODO: showAllClients();
                     break;
                 case 3:
-                   // findGymProfileByName();
+                   // TODO: findGymProfileByName();
                     break;
                 case 4:
                     Trainer.createTrainerRoutine(trainerID);
                     break;
                 case 5:
-                    //Trainer.showAllRoutines();
+                    //TODO: Trainer.showAllRoutines();
                     break;
                 case 6:
                     Trainer.addRoutineToProfile();
@@ -199,21 +268,85 @@ public class Main {
 
     }
 
-    public static void MgmtUI()
-    {
-        System.out.println(" \n \n Welcome to your Management dashboard!");
+    public static void MgmtUI() {
+        System.out.println("\n\nWelcome to your Management dashboard!");
 
-        //view day's schedule
+        int choice = -1;
 
-        //make a group class and add to sched
+        while (choice != 0) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. View day's schedule");
+            System.out.println("2. Make a group class and add to schedule");
+            System.out.println("3. Update a class");
+            System.out.println("4. Equipment management");
+            System.out.println("0. Exit");
 
-        //update a class
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
 
-        // Equipment management
-            //1)View all repair tickets
-            //2) Add repair ticket
-            //3) remove repair ticket
+            switch (choice) {
+                case 1:
+                    // TODO: View day's schedule
+                    System.out.println("Viewing day's schedule...");
+                    break;
+                case 2:
+                    // TODO: Make a group class and add to schedule
+                    System.out.println("Making a group class and adding to schedule...");
+                    break;
+                case 3:
+                    // TODO: Update a class
+                    System.out.println("Updating a class...");
+                    break;
+                case 4:
+                    EquipMaintenance();
+                    break;
+                case 0:
+                    System.out.println("Exiting Management dashboard...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
 
+    public static void EquipMaintenance() {
+        System.out.println("\nEquipment Maintenance:");
 
+        int choice = -1;
+
+        while (choice != 0) {
+            System.out.println("\nChoose an option:");
+            System.out.println("1. View all repair tickets");
+            System.out.println("2. Add repair ticket");
+            System.out.println("3. Remove repair ticket");
+            System.out.println("0. Exit");
+
+            System.out.print("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    // TODO: View all repair tickets
+                    System.out.println("Viewing all repair tickets...");
+                    break;
+                case 2:
+                    // TODO: Add repair ticket
+                    System.out.println("Adding repair ticket...");
+                    break;
+                case 3:
+                    // TODO: Remove repair ticket
+                    System.out.println("Removing repair ticket...");
+                    break;
+                case 0:
+                    System.out.println("Exiting Equipment Maintenance...");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
