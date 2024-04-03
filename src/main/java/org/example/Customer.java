ac0321362a121c7b2d9cddc051c07f38be7e0320
ahmedsayedsharafeldin <163793637+ahmedsayedsharafeldin@users.noreply.github.com>
2024-04-03 19:56:33 +0400

    rebased from main

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -1,5 +1,43 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class Customer
 {
+
+    public static int customerSignIn(String username, String password)
+    {
+        Connection conn = Main.dbConnection;
+
+        String sql = "SELECT id FROM Customer WHERE username = ? AND password = ?";
+
+        try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+
+            System.out.println(pstmt);
+
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                int customerId = rs.getInt("id");
+                System.out.println("Login successful!");
+                return customerId;
+            } else {
+                System.out.println("Incorrect username or password.");
+                return -1;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1; // Return -1 if an error occurs
+    }
+
+    //Display all customer profiles
 }
